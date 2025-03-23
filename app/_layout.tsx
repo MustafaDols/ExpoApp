2262d9d2263e5a21d6e2c396ac62f86f3fbeb716
Mustafa Mahmoud AbdElaziz <mustafadolseka491@gmail.com>
2025-03-23 22:01:04 +0200

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="login" options={{ title: "Login" }} />
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="About" options={{ title: "About" }} />
+      <Stack.Screen name="scroll-view" options={{ title: "Scroll View" }} />
+      <Stack.Screen name="flat-list" options={{ title: "Flat List" }} />
+    </Stack>
+  );
 }
